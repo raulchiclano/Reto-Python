de130b8faeda6f5b7973fc48e2e5a2061ce5da87
raulchiclano <raulchiclano@gmail.com>
2022-02-20 09:29:01 +0100

    Mejorado el output

--- Reto_1/src/main.sh
+++ Reto_1/src/main.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 user=$(whoami)
-dir=$(ls ~/ | grep -e Descargas -e descargas -e downloads -e Downloads )
+dir=$(ls ~/ | grep -e Descargas -e descargas -e downloads -e Downloads)
 
-echo -e "Directorio: "/home"/$user/$dir","\n",$(ls -p ~/$dir | grep -v /) | tr ' ' '\n'
+echo -e "Directorio: "/home"/$user/$dir\n" && find ~/$dir -maxdepth 1 -type f | awk -F/ '{print $5}'
+
+
