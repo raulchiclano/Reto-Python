411283a91b0e3128017c503c11f6419dedc7f512
raulchiclano <raulchiclano@gmail.com>
2022-06-25 18:08:55 +0200

    Eliminindo version en bash del Reto 1

--- Reto_1/src/main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-USER=$(/usr/bin/whoami)
-DIR=$(ls ~/ | grep -e Descargas -e descargas -e downloads -e Downloads)
-
-echo -e "Directorio: "/home"/$USER/$DIR\n" && find ~/$DIR -maxdepth 1 -type f | awk -F/ '{print $5}'
-
-
