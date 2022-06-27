740b9a06da5eb3ac6e14885d3457e0f49590295a
raulchiclano <raulchiclano@gmail.com>
2022-06-27 16:55:01 +0200

    Terminado Reto 2

--- Reto_2/src/main.py
+++ Reto_2/src/main.py
@@ -1,2 +1,37 @@
  #!/usr/bin/env python3
+ # -*- coding: utf-8 -*- #!/usr/bin/env python3
  # -*- coding: utf-8 -*-
+
+import os
+import getpass
+import re
+
+payload = ("jpge", "JPGE", "jpg", "JPG", "PNG", "png")
+
+def main():
+    user = getpass.getuser()
+    directory = "/home/"+user+"/"
+    
+    
+    target = ["Descargas", "descargas", "downloads", "Downloads"]
+
+
+
+    with os.scandir(directory) as ficheros:
+        ficheros = [fichero.name for fichero in ficheros if fichero.is_dir()]
+
+    for name in target:
+        if name in ficheros:
+            download_directory = "/home/"+user+"/"+name
+            with os.scandir(download_directory) as ficheros:
+                ficheros = [fichero.name for fichero in ficheros if fichero.is_file()]
+                print("[+] Directorio: "+download_directory+"\n")
+                for f in ficheros:
+                    if f.endswith(payload):
+                        print("    > ",f)
+
+if __name__ == "__main__":
+  main()
+
+"""
+"""
